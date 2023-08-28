7df9f750d59b3f672e9b007bd1cc7075f8cb8c60
Sam <samibluth@gmail.com>
2023-08-28 11:00:33 +0300

    Generating transient token

--- routes/flexRoute.js
+++ routes/flexRoute.js
@@ -3,5 +3,5 @@ const router = require('express').Router();
 const generateCaptureContext = require('../controllers/microformController')
 
 router.post('/captureContext', generateCaptureContext.generateCaptureContext)
-
+router.post('/captureContextSdk', generateCaptureContext.captureContextFromSdk)
 module.exports = router;
