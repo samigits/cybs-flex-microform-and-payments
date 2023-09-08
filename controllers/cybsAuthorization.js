67c413ec131329b04a1decedca60597d8fa45815
Sam <samibluth@gmail.com>
2023-09-08 10:33:20 +0300

    authenticate and pay with transient token

--- controllers/cybsAuthorization.js
+++ controllers/cybsAuthorization.js
@@ -45,7 +45,7 @@ exports.paymentAuthorization = async (req, res, next) => {
     request.accept(acceptType);
 
     request.end((err, response) => {
-      console.log("auth response", response)
+      console.log("auth response", response);
       var data = response.body ? response.body : response.text;
       if (
         data === null ||
@@ -64,8 +64,101 @@ exports.paymentAuthorization = async (req, res, next) => {
       res.json({
         ok: true,
         header: response.header,
-        data: data ? data :JSON.parse(response.text),
+        data: data ? data : JSON.parse(response.text),
       });
     });
   } catch (err) {}
+};
+
+exports.authWithTransientToken = (req, res, next) => {
+  try {
+    var payload = {
+      clientReferenceInformation: {
+        code: "TC50171_3",
+      },
+      orderInformation: {
+        amountDetails: {
+          totalAmount: "102.21",
+          currency: "USD",
+        },
+        billTo: {
+          firstName: "RTS",
+          lastName: "VDP",
+          address1: "201 S. Division St.",
+          locality: "Ann Arbor",
+          administrativeArea: "MI",
+          postalCode: "48104-2201",
+          country: "US",
+          district: "MI",
+          buildingNumber: "123",
+          email: "test@cybs.com",
+          phoneNumber: "999999999",
+        },
+      },
+      tokenInformation: {
+        transientTokenJwt:
+          "eyJraWQiOiIwMFN2SWFHSWZ5YXc4OTdyRGVHOWVGZE9ES2FDS2MxcSIsImFsZyI6IlJTMjU2In0.eyJpc3MiOiJGbGV4LzAwIiwiZXhwIjoxNjE0NzkyNTQ0LCJ0eXBlIjoiYXBpLTAuMS4wIiwiaWF0IjoxNjE0NzkxNjQ0LCJqdGkiOiIxRDBWMzFQMUtMRTNXN1NWSkJZVE04VUcxWE0yS0lPRUhJVldBSURPkhLNjJJSFQxUVE1NjAzRkM3NjA2MDlDIn0.FrN1ytYcpQkn8TtafyFZnJ3dV3uu1XecDJ4TRIVZN-jpNbamcluAKVZ1zfdhbkrB6aNVWECSvjZrbEhDKCkHCG8IjChzl7Kg642RWteLkWz3oiofgQqFfzTuq41sDhlIqB-UatveU_2ukPxLYl87EX9ytpx4zCJVmj6zGqdNP3q35Q5y59cuLQYxhRLk7WVx9BUgW85tl2OHaajEc25tS1FwH3jDOfjAC8mu2MEk-Ew0-ukZ70Ce7Zaq4cibg_UTRx7_S2c4IUmRFS3wikS1Vm5bpvcKLr9k_8b9YnddIzp0p0JOCjXC_nuofQT7_x_-CQayx2czE0kD53HeNYC5hQ",
+      },
+    };
+    var trxPayload = JSON.stringify(payload);
+    var resource = "/pts/v2/payments";
+    var method = "post";
+    var statusCode = -1;
+    var url = "https://apitest.cybersource.com" + resource;
+
+    var headerParams = {};
+    var contentType = "application/json;charset=utf-8";
+    var acceptType = "application/hal+json;charset=utf-8";
+    var request = superagent(method, url);
+    var bodyParam = trxPayload;
+
+    var signature = generateHttpSignature(resource, method, bodyParam);
+    var date = new Date(Date.now()).toUTCString();
+    var digest = digestGenerator(trxPayload);
+    digest = "SHA-256=" + digest;
+
+    headerParams["digest"] = digest;
+    headerParams["v-c-merchant-id"] = config.merchant.merchantId;
+    headerParams["date"] = date;
+    headerParams["host"] = config.runEnvironment;
+    headerParams["signature"] = signature;
+    headerParams["User-Agent"] = "Mozilla/5.0";
+    //set header parameters
+    request.set(normalizeParams(headerParams));
+
+    //set timeout
+    request.timeout(5000);
+
+    request.type(contentType);
+
+    request.send(bodyParam);
+
+    request.accept(acceptType);
+
+    request.end((err, response) => {
+      console.log("auth response", response);
+      var data = response.body ? response.body : response.text;
+      if (
+        data === null ||
+        (typeof data === "object" &&
+          typeof data.length === "undefined" &&
+          !Object.keys(data).length)
+      ) {
+        // SuperAgent does not always produce a body; use the unparsed response as a fallback
+        data = response.text;
+      }
+
+      var _status = -1;
+      if (response["status"] >= 200 && response["status"] <= 299) {
+        _status = 0;
+      }
+      res.json({
+        ok: true,
+        header: response.header,
+        data: data ? data : JSON.parse(response.text),
+      });
+    });
+  } catch (err) {
+    console.log("Auth with TntToken:", err);
+  }
 };
